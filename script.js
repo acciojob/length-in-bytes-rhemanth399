806c828fe1842ba0e9976858db69f316964253d1
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-05-01 09:45:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const byteSize = (str) => {
   // write your code here
+	
+  // Convert the string to a UTF-8 encoded byte array
+  const byteArray = new TextEncoder().encode(str);
+  
+  // Return the length of the byte array
+  return byteArray.length;
+}
+
 };
 
 // Do not change the code below
